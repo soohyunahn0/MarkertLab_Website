e448ab7e486caa88489d41793bde7561da975e44
soohyunahn0 <soohyunahn0@gmail.com>
2025-03-11 20:27:56 -0500

    Add all my files so far to github

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+app.secret_key = "gUG*7BNmM*[*hUd7&y6hb}GlTcub`C"
+
+@app.route("/home")
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/people")
+def people():
+    return render_template("people.html")
+
+@app.route("/research")
+def research():
+    return render_template("research.html")
